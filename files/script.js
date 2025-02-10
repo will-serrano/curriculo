9367a6e41f412279507ee06a5bf53231ab86d5f5
Willian Serrano <38846721+will-serrano@users.noreply.github.com>
2025-02-10 00:22:53 -0300

    Feature/data optimization (#15)
    * Adjusted professional summary for clarity and accuracy.
    
    * Update contact information and enhance professional summaries in multiple languages
    
    * Remove HTML and CSS files for the BTG curriculum feature
    
    * Add console log for technology counts in script.js
    
    * Add technology categories and pie chart functionality to curriculum module
    
    ---------
    
    Co-authored-by: Willian Serrano <will.serrano@carameloproductions.com>

--- files/script.js
+++ files/script.js
@@ -1,9 +1,8 @@
-const { jsPDF } = window.jspdf;
-
 (function () {
     const CurriculoModule = (function () {
         let currentLanguage = "pt-br"; // Idioma padrão
         let currentTheme = "light"; // Tema padrão
+        let techCount = {}; // Move techCount to the outer scope
 
         // Função para carregar os dados do JSON do idioma selecionado
         const fetchData = async (language) => {
@@ -31,24 +30,25 @@ const { jsPDF } = window.jspdf;
                 container.appendChild(createSection("experience", buildExperience(data)));
                 container.appendChild(createSection("education", buildEducation(data)));
                 container.appendChild(createSection("language", buildLanguage(data)));
+                createPieChart(techCount, data); // Passa o objeto data para createPieChart
             }
         };
 
         // Funções de Construção de Seções
         const buildHeader = (data) => `
-            <h1>${data.nome}</h1>
-            <h2>${data.titulo}</h2>
+            <h1>${data.name}</h1>
+            <h2>${data.title}</h2>
         `;
 
         const buildContact = (data) => `
-            <p>${data.contato.localizacao} | ${data.contato.telefone} | 
-            <a href="mailto:${data.contato.email}">${data.contato.email}</a></p>
+            <p>${data.contact.location} | ${data.contact.availability} | 
+            <a href="mailto:${data.contact.email}">${data.contact.email}</a></p>
         `;
 
         const buildSkills = (data) => {
-            let skillsHTML = `<h3>${data.titulos.competencias}</h3><div class="skills">`;
+            let skillsHTML = `<h3>${data.titles.skills}</h3><div class="skills">`;
         
-            data.competencias.forEach(skill => {
+            data.skills.forEach(skill => {
                 skillsHTML += `<span>${skill}</span>`;
             });
         
@@ -57,39 +57,81 @@ const { jsPDF } = window.jspdf;
         };
 
         const buildSummary = (data) => `
-            <h3>${data.titulos.resumo}</h3>
-            <p>${data.resumo}</p>
+            <h3>${data.titles.summary}</h3>
+            <p>${data.summary}</p>
         `;
 
         const buildExperience = (data) => {
-            let experienceHTML = `<h3>${data.titulos.experiencia}</h3>`;
-            data.experiencias.forEach(job => {
+            let experienceHTML = `<h3>${data.titles.experience}</h3>`;
+            techCount = {}; // Reset techCount
+            const ungroupedTechnologies = [];
+            const unusedTechnologies = [];
+
+            // Inicializa o contador de tecnologias
+            Object.keys(data['technology-categories']).forEach(category => {
+                const translatedCategory = data['report-titles'][category] || category;
+                techCount[translatedCategory] = [];
+                data['technology-categories'][category].forEach(tech => {
+                    techCount[translatedCategory].push({ name: tech, count: 0 });
+                });
+            });
+
+            data.experiences.forEach(job => {
                 experienceHTML += `
                     <div class="job">
-                        <h4>${job.empresa} | ${job.cargo}</h4>
-                        <p><strong>${data.titulos.periodo}:</strong> ${job.periodo}</p>
+                        <h4>${job.company} | ${job.position}</h4>
+                        <p><strong>${data.titles.period}:</strong> ${job.period}</p>
                         <ul>
-                            ${job.descricao.map(item => `<li>${item}</li>`).join('')}
+                            ${job.description.map(item => `<li>${item}</li>`).join('')}
                         </ul>
-                        <p><strong>${data.titulos.tecnologia}:</strong></p>
+                        <p><strong>${data.titles.technology}:</strong></p>
                         <div class="technologies">
-                            ${job.tecnologias.map(tech => `<span>${tech}</span>`).join('')}
+                            ${job.technologies.map(tech => {
+                                let found = false;
+                                Object.keys(techCount).forEach(category => {
+                                    techCount[category].forEach(techObj => {
+                                        if (techObj.name === tech) {
+                                            techObj.count += 1;
+                                            found = true;
+                                        }
+                                    });
+                                });
+                                if (!found) {
+                                    ungroupedTechnologies.push(tech);
+                                }
+                                return `<span>${tech}</span>`;
+                            }).join('')}
                         </div>
                     </div>
                 `;
             });
+
+            // Lista as categorias-tecnologias não utilizadas
+            Object.keys(data['technology-categories']).forEach(category => {
+                const translatedCategory = data['report-titles'][category] || category;
+                techCount[translatedCategory].forEach(tech => {
+                    if (tech.count === 0) {
+                        unusedTechnologies.push(`${tech.name} (${translatedCategory})`);
+                    }
+                });
+            });
+
+            console.log('Ungrouped Technologies:', ungroupedTechnologies); // Para verificar as tecnologias não agrupadas no console
+            console.log('Academic or Personal Knowledge:', unusedTechnologies); // Para verificar as tecnologias não utilizadas no console
+            console.log('Technology Counts:', techCount); // Exibe a contagem de tecnologias no console
+
             return experienceHTML;
         };
-        
+
         const buildLanguage = (data) => {
             // Adiciona o título da seção
-            let languageHTML = `<h3>${data.titulos.idiomas}</h3>`;
+            let languageHTML = `<h3>${data.titles.languages}</h3>`;
         
             // Itera sobre os idiomas no JSON e cria um bloco para cada um
-            data.idiomas.forEach(lang => {
+            data.languages.forEach(lang => {
                 languageHTML += `
                     <div class="language-item">
-                        <p><strong>${lang.idioma}</strong> - ${lang.nivel}</p>
+                        <p><strong>${lang.language}</strong> - ${lang.level}</p>
                     </div>
                 `;
             });
@@ -99,13 +141,13 @@ const { jsPDF } = window.jspdf;
 
         const buildEducation = (data) => {
             // Adiciona o título da seção
-            let educationHTML = `<h3>${data.titulos.educacao}</h3>`;
+            let educationHTML = `<h3>${data.titles.education}</h3>`;
         
             // Itera sobre as instituições no JSON e cria um bloco para cada uma
-            data.educacao.forEach(edu => {
+            data.education.forEach(edu => {
                 educationHTML += `
                     <div class="education-item">
-                        <p><strong>${edu.instituicao}</strong> - ${edu.curso}</p>
+                        <p><strong>${edu.institution}</strong> - ${edu.course}</p>
                     </div>
                 `;
             });
@@ -122,6 +164,81 @@ const { jsPDF } = window.jspdf;
             return section;
         };
 
+        // Função para criar um gráfico de pizza
+        const createPieChart = (techCount, data) => {
+            const container = document.querySelector("#curriculo");
+            const existingCanvas = document.querySelector('#techPieChart');
+            if (existingCanvas) {
+                existingCanvas.remove();
+            }
+
+            const canvas = document.createElement('canvas');
+            canvas.id = 'techPieChart';
+            container.appendChild(canvas);
+
+            const ctx = canvas.getContext('2d');
+            const categories = Object.keys(techCount).map(category => data['report-titles'][category] || category).filter(Boolean);
+            const chartData = categories.map(category => {
+                const techCategory = techCount[category];
+                return techCategory ? techCategory.reduce((sum, tech) => sum + tech.count, 0) : 0;
+            });
+
+            const colors = [
+                // Cores primárias
+                '#FF0000', // Vermelho
+                '#0000FF', // Azul
+                '#FFFF00', // Amarelo
+                
+                // Cores secundárias (misturas das primárias)
+                '#00FF00', // Verde (Amarelo + Azul)
+                '#FF00FF', // Magenta (Vermelho + Azul)
+                '#00FFFF', // Ciano (Azul + Verde)
+            
+                // Cores terciárias e complementares
+                '#800000', // Marrom avermelhado
+                '#808000', // Verde-oliva
+                '#008000', // Verde escuro
+                '#800080', // Roxo
+                '#008080', // Verde-azulado
+                '#FFA500', // Laranja
+                '#FFC0CB', // Rosa claro
+                '#DC143C', // Carmesim
+                '#8A2BE2', // Azul violeta
+                '#D2691E', // Chocolate
+                '#2E8B57', // Verde mar profundo
+                '#4682B4', // Azul aço
+                '#C9CBCF', // Cinza claro
+                '#000000'  // Preto
+            ];
+
+            if (categories.length > 0 && chartData.length > 0) {
+                new Chart(ctx, {
+                    type: 'pie',
+                    data: {
+                        labels: categories,
+                        datasets: [{
+                            data: chartData,
+                            backgroundColor: colors.slice(0, categories.length),
+                        }]
+                    },
+                    options: {
+                        responsive: true,
+                        plugins: {
+                            legend: {
+                                position: 'top',
+                            },
+                            title: {
+                                display: true,
+                                text: 'Technology Usage by Category'
+                            }
+                        }
+                    }
+                });
+            } else {
+                console.warn('No data available for the pie chart.');
+            }
+        };
+
         // Alternar Idioma
         const changeLanguage = (language) => {
             currentLanguage = language;
@@ -134,70 +251,10 @@ const { jsPDF } = window.jspdf;
             document.body.className = theme;
         };
 
-        const downloadPDF = async () => {
-            const doc = new jsPDF({
-                orientation: "portrait",
-                unit: "mm",
-                format: "a4"
-            });
-        
-            const data = await fetchData(currentLanguage);
-            let yPosition = 20;
-        
-            doc.setFont("Helvetica", "bold");
-            doc.setFontSize(16);
-            doc.text(data.nome, 105, yPosition, { align: "center" });
-            yPosition += 10;
-        
-            doc.setFont("Helvetica", "normal");
-            doc.setFontSize(14);
-            doc.text(data.titulo, 105, yPosition, { align: "center" });
-            yPosition += 20;
-        
-            doc.setFontSize(12);
-            doc.text("Resumo Profissional:", 10, yPosition);
-            yPosition += 10;
-            const resumo = doc.splitTextToSize(data.resumo, 180);
-            doc.text(resumo, 10, yPosition);
-            yPosition += resumo.length * 8;
-        
-            doc.text("Experiência Profissional:", 10, yPosition);
-            yPosition += 10;
-        
-            data.experiencias.forEach((job) => {
-                doc.setFont("Helvetica", "bold");
-                doc.text(`${job.empresa} | ${job.cargo}`, 10, yPosition);
-                yPosition += 8;
-        
-                doc.setFont("Helvetica", "normal");
-                doc.text(`Período: ${job.periodo}`, 10, yPosition);
-                yPosition += 8;
-        
-                doc.text("Descrição:", 10, yPosition);
-                yPosition += 8;
-                job.descricao.forEach((desc) => {
-                    doc.text(`- ${desc}`, 10, yPosition);
-                    yPosition += 8;
-                });
-        
-                doc.text("Tecnologias Utilizadas:", 10, yPosition);
-                yPosition += 8;
-                job.tecnologias.forEach((tech) => {
-                    doc.text(`- ${tech}`, 10, yPosition);
-                    yPosition += 8;
-                });
-        
-                yPosition += 10; // Espaço entre experiências
-            });
-        
-            doc.save(`${data.nome.replaceAll(" ", "_")}_curriculo.pdf`);
-        };      
-
         return {
             renderCurriculo,
             changeLanguage,
-            changeTheme,
-            downloadPDF
+            changeTheme
         };
     })();
 
@@ -213,7 +270,4 @@ const { jsPDF } = window.jspdf;
         CurriculoModule.changeTheme(event.target.value);
     });
 
-    document.querySelector("#download-pdf").addEventListener("click", () => {
-        CurriculoModule.downloadPDF();
-    });
 })();
